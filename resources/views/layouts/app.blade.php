5e95bcf1d84f3bfdf34430f3a12990915ab52093
Beatriz <galgrabe@gmail.com>
2017-01-24 19:59:27 +0100

    Prueba de bootstrap_theme  -

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -25,6 +25,7 @@
         <!-- Styles -->
         <link rel="stylesheet" type="text/css" href="css/bootstrap.min.css"/>
         <link rel="stylesheet" type="text/css" href="css/font-awesome.min.css">
+        <link rel="stylesheet" type="text/css" href="css/bootstrap_theme_slate.min.css">
         <!--<link href="https://fonts.googleapis.com/css?family=Lato:100,300,400,700" rel='stylesheet' type='text/css'>
         <link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" rel="stylesheet">-->
    
