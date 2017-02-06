4938a4efab1e68fb6a001e92d5240b875b0c7db1
davidac23 <d.almirall23@gmail.com>
2017-02-06 06:40:35 +0100

    Preguntas
    Apartado de preguntas

--- resources/views/preguntas.blade.php
+++ resources/views/preguntas.blade.php
@@ -8,7 +8,27 @@
     <div class="container ">
         <div class="row ">
             <div class="col-md-10 ">
-                <!--//Aqui-->
+                
+                <div class="preguntas">
+                    <h1>Pregunta del juego</h1>
+                        <div class= "respuestas">
+                            <div class="radio">
+                              <label><input type="radio" name="optradio">Respuesta 1</label>
+                            </div>
+                            <div class="radio">
+                              <label><input type="radio" name="optradio">Respuesta 2</label>
+                            </div>
+                            <div class="radio">
+                              <label><input type="radio" name="optradio" >Respuesta 3</label>
+                            </div>
+                            <div class="radio">
+                              <label><input type="radio" name="optradio" >Respuesta 4</label>
+                            </div>
+                                <br>
+                        </div>
+                    <a href="#" class="btn btn-default">Validar</a>                      
+                        
+                </div>
             </div>
         </div>
     </div>
